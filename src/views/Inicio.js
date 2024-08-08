6c4932ee567fa49d41016675b920a8e169d86b1e
isai66 <148422646+isai66@users.noreply.github.com>
2024-08-08 15:09:09 -0600

    Update Inicio.js 23

--- src/views/Inicio.js
+++ src/views/Inicio.js
@@ -74,7 +74,7 @@ const Inicio = () => {
                         {randomProducts.map((product) => (
                             <div className="tarjeta" key={product.ID_Prenda}>
                             <img
-                                src={`http://alevosia.host8b.me/image/${product.Imagen}`}
+                                src={`https://bucketpractica1apl.s3.us-east-2.amazonaws.com/${product.Imagen}`}
                                 alt={product.Nombre}
                                 className="imagen-producto"
                             />
