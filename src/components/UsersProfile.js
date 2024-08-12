af75e30223c0cbdb6a59be28a9a3a74f05e34dac
isai66 <148422646+isai66@users.noreply.github.com>
2024-08-12 10:38:45 -0600

    Update UsersProfile.js

--- src/components/UsersProfile.js
+++ src/components/UsersProfile.js
@@ -13,7 +13,7 @@ const UsersProfile = () => {
     // Fetch users from the server
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('http://localhost:3001/usersNormal'); // Adjust the URL as needed
+        const response = await axios.get('https://alev-backend-vercel.vercel.app/usersNormal'); // Adjust the URL as needed
         setUsers(response.data);
         setFilteredUsers(response.data);
       } catch (error) {
@@ -120,7 +120,7 @@ const UsersProfile = () => {
 
 const handleConvertToUser = async (userId) => {
   try {
-    await axios.post(`http://localhost:3001/convertToUser/${userId}`);
+    await axios.post(`https://alev-backend-vercel.vercel.app/convertToUser/${userId}`);
     alert('Usuario convertido a empleado.');
     // Optionally, you can refresh the user list here
   } catch (error) {
