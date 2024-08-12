53341e799bcb12800a4072867ecc862d584d2c2f
isai66 <148422646+isai66@users.noreply.github.com>
2024-08-12 11:40:20 -0600

    Update Women.js

--- src/views/Women.js
+++ src/views/Women.js
@@ -47,7 +47,7 @@ const Women = () =>{
                         {products.map((product) => (
                             <div className="tarjeta" key={product.ID_Prenda}>
                             <img
-                                src={`https://alevosia.host8b.me/image${product.Imagen}`}
+                                src={`https://alevosia.host8b.me/image/${product.Imagen}`}
                                 alt={product.Nombre}
                                 className="imagen-producto"
                             />
